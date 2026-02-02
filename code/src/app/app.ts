d62c833a635334ce8a9962c064f46a5c6f4860ef
Artur Dias <arturdrm@hotmail.com>
2026-02-02 15:09:02 -0300

    feat: add back button functionality with styling and layout adjustments

--- code/src/app/app.ts
+++ code/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  private location = inject(Location);
+
+  goBack(): void {
+    this.location.back();
+  }
 }
