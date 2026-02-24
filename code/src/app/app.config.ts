df86e25e0d946317a35ec235d29ea48356bc1fa7
Artur Dias <arturdrm@hotmail.com>
2026-02-24 17:05:17 -0300

    feat: add service worker support and update app manifest for PWA capabilities

--- code/src/app/app.config.ts
+++ code/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 import { provideTransloco } from '@jsverse/transloco';
 import { registerLocaleData } from '@angular/common';
 import localePtBr from '@angular/common/locales/pt';
+import { Capacitor } from '@capacitor/core';
 
 import { routes } from './app.routes';
 import { TranslocoHttpLoader } from './transloco-loader';
@@ -23,6 +25,10 @@ export const appConfig: ApplicationConfig = {
                 prodMode: !isDevMode(),
             },
             loader: TranslocoHttpLoader,
+        }),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode() && !Capacitor.isNativePlatform(),
+            registrationStrategy: 'registerWhenStable:30000',
         }),
     ]
 };
